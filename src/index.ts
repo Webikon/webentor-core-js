0392d8aa954d0f90475f7b8785b12382ef94b5fd
Pavol Caban <pavol.caban@webikon.sk>
2025-03-24 14:50:45 +0100

    Fix utils export

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ export { useBlockParent, usePostTypes, useTaxonomies } from './blocks-utils';
 
 export { Alpine } from './_alpine';
 export { SliderComponent, Swiper } from './_slider';
-export { setImmutably, debounce, throttle } from './_utils';
+export { setImmutably, debounce, throttle, camelize } from './_utils';
 
 export type { ButtonAttributes } from './types/_block-components';
 export type { WebentorConfig } from './types/_webentor-config';
