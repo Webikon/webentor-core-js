0102944fe8428b828b2533ffe3b0710186d2e32e
Pavol Caban <pavol.caban@webikon.sk>
2025-04-17 10:34:27 +0200

    Fix Alpine init for better compatibility

--- src/_alpine.ts
+++ src/_alpine.ts
@@ -1,14 +1,20 @@
 import collapse from '@alpinejs/collapse';
 import Alpine from 'alpinejs';
 
-// Register default plugins
-Alpine.plugin(collapse);
+// Initialize Alpine if it's not already initialized
+const AlpineInstance = window.Alpine || Alpine;
+if (!window.Alpine) {
+  // Make Alpine globally available
+  window.Alpine = Alpine;
 
-// Make Alpine globally available
-window.Alpine = Alpine;
+  document.addEventListener('DOMContentLoaded', () => {
+    AlpineInstance.start();
+  });
+}
 
-document.addEventListener('DOMContentLoaded', () => {
-  Alpine.start();
+document.addEventListener('alpine:init', () => {
+  // Register default plugins
+  AlpineInstance.plugin(collapse);
 });
 
-export { Alpine };
+export { AlpineInstance as Alpine };
